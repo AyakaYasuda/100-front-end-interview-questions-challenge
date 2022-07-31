8fa8e0e9e571900ec99ea512afb68e78ffad67ba
AyakaYasuda <ayakayasuda23@gmail.com>
2022-07-31 16:54:52 -0700

    added memo about falsey values

---
+++ 01.JavaScript/falsey-value.js
@@ -0,0 +1,22 @@
+/*
+
+What are falsey values in JavaScript?
+- There are only 6 falsey values in JS: undefined, null, NaN, 0, ""(empty string), false.
+
+*/
+
+const example1 = undefined; // undefined
+const example2 = null; // object
+const example3 = NaN; // number
+const example4 = 0;
+const example5 = '';
+const example6 = false;
+const example7 = 1;
+
+console.log(!!example1);
+console.log(!!example2);
+console.log(!!example3);
+console.log(!!example4);
+console.log(!!example5);
+console.log(!!example6);
+console.log(!!example7);
