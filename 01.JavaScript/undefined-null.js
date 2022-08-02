b55cbb196e361743e302b3b65867d5dfd28fab61
AyakaYasuda <ayakayasuda23@gmail.com>
2022-08-02 04:44:26 -0700

    added memo about the difference between undefined and null

---
+++ 01.JavaScript/undefined-null.js
@@ -0,0 +1,19 @@
+/*
+
+What is the difference between undefined and null?
+- Undefined means that a variable has not been assigned a value. The system will return undefined.
+- Null is an assigned value saying the variable doesn't yet have a value.
+
+*/
+
+const user1 = {};
+
+let user2 = null;
+
+console.log(user1);
+console.log(user1.firstName);
+console.log(null);
+console.log(user3);
+
+let a;
+console.log(a); // undefined
