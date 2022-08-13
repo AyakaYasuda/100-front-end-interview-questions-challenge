69600f1e0d2f1c947b2289772b27e7da5d71144e
AyakaYasuda <ayakayasuda23@gmail.com>
2022-08-12 20:20:46 -0700

    added memo about asynchronous, promise, and timers

---
+++ 02.Async-JavaScript/promise.js
@@ -0,0 +1,49 @@
+/*
+
+What are a promise?
+
+- A promise is an object returned by an asynchronous function, which allows you to write asynchronous program more easily and readable.
+- A Promise can be three types of state: pending, fulfilled, rejected.
+- A Promise takes a callback function and it takes tho parameters: resolve and reject
+- When a promise is fulfilled, resolve is executed and the code inside then is executed.
+- When something wrong happens and a promise is rejected, reject is executed and the code inside catch is executed.
+
+*/
+
+new Promise((resolve, reject) => {
+  resolve('hello');
+  reject('something wrong happened');
+})
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
+  .finally(() => console.log('finally'));
+
+// You can also call an asynchronous function in the callback
+new Promise((resolve) => {
+  console.log('Promise');
+  setTimeout(() => {
+    resolve('hello');
+  }, 1000);
+})
+  .then((data) => {
+    console.log('then 1 - ', data);
+    return data;
+  })
+  .then((data) => {
+    console.log('then 2 - ', data);
+    throw new Error('oops');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log('finally');
+  });
+
+console.log('global end');
+
+// Promise
+// global end
+// then 1 - hello
+// then 2 - hello
+// finally
